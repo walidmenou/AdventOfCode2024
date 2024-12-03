cd51f027b3988dd028b231da90cccd56e2b7a437
Walid Menou <walid@MacBook-Pro-de-Walid.local>
2024-12-02 22:04:22 -0500

    Initial commit

---
+++ aoc1.py
@@ -0,0 +1,48 @@
+def parseInput(filename="/Users/walid/Documents/AdventOfCode2024/input.txt"):
+
+    list1 = []
+    list2 = []
+    
+    with open(filename, "r") as file:
+        for line in file:
+            numbers = line.split()
+            list1.append(int(numbers[0]))
+            list2.append(int(numbers[1]))
+    
+    return list1, list2
+
+def findDistance():
+    l1, l2 = parseInput()
+    
+    l1 = sorted(l1)
+    l2 = sorted(l2)
+    
+    res = 0
+    for i, j in zip(l1, l2):
+        res += abs(i - j)
+    
+    return res
+
+def findSimilarity():
+    l1, l2 = parseInput()
+
+    l1 = sorted(l1)
+    l2 = sorted(l2)
+
+    count_l2 = {}
+
+    for n in l2:
+        if n in count_l2:
+            count_l2[n] += 1
+        else:
+            count_l2[n] = 1
+    
+    similarity = 0
+
+    for n in l1:
+        if n in count_l2:
+            similarity += n*count_l2[n]
+
+    return similarity
+
+print(findSimilarity())
